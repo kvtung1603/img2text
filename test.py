0111feb89548fb5c2dd52827ade4d00de151b248
kvtung1603 <50308924+kvtung1603@users.noreply.github.com>
2021-11-24 16:32:05 +0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from Vocab import Vocab
-from model.OCR import OCR
-
-chars = 'aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
-
-vocab = Vocab(chars)
-
-model = OCR(len(vocab))
-print(model)
